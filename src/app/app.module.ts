7011be2baeaa9cfbd18f2fd05e88867ef6a396af
hieu <phuchieuct93@gmail.com>
2020-01-11 19:28:34 +0700

    Update content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInformationComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,7 @@ registerLocaleData(en);
     NgZorroAntdModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
